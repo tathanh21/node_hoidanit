eab24f059b79c790ea66f2d572b8f17b23b78895
tathanh21 <tatthanhk50pt@gmail.com>
2023-06-07 11:17:52 +0700

    vd13

--- src/route/web.js
+++ src/route/web.js
@@ -6,6 +6,9 @@ const initWebRoute = (app) => {
     router.get('/', homeController.getHomepage)
     router.get('/detail/user/:id', homeController.getDetailpage)
     router.post('/create-new-user', homeController.createNewUser)
+    router.post('/delete-user', homeController.deleteUser)
+    router.get('/edit-user/:id', homeController.getEditPage)
+    router.post('/update-user', homeController.updateUser)
     return app.use('/', router);
 }
 
